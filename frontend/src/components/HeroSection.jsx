75aa4c28ff410b20c86544635f44c3b52fad2ad2
Reef Kenig <rkenig@aol.com>
2025-11-04 00:13:38 +0200

    Replaced video

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,15 +1,14 @@
 import "../styles/videoSection.css";
 import logoHeart from "../assets/images/Vme_logo-heart.png";
 
+const VIDEO =
+  "https://vme-landingpage-videos.s3.eu-north-1.amazonaws.com/newIntro.mp4";
+
 export default function VideoSection() {
   return (
     <section className="video-section">
       <div className="video-container">
-        <video
-          src="https://vme-landingpage-videos.s3.eu-north-1.amazonaws.com/intro+vme.mp4"
-          controls
-          className="video"
-        ></video>
+        <video src={VIDEO} controls className="video"></video>
       </div>
 
       <div className="text-container">
